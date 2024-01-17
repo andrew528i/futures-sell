981b4a583bb6a6b0776a98aa4da21b2ce7553e31
Андрей Рыбалко <debiannuser@gmail.com>
2024-01-17 19:45:25 +0300

    fixes

--- main.go
+++ main.go
@@ -19,53 +19,33 @@ const (
 )
 
 func main() {
-	//// deal with timestamp and sleep
-	//now := time.Now()
-	//nowTs := now.UnixMilli()
+	client := binance.NewFuturesClient(ApiKey, ApiSecret)
 
-	defer func() {
-		client := binance.NewFuturesClient(ApiKey, ApiSecret)
-		err := client.NewCancelAllOpenOrdersService().Symbol(OrderSymbol).Do(context.Background())
+	defer func(c *futures.Client) {
+		err := c.NewCancelAllOpenOrdersService().Symbol(OrderSymbol).Do(context.Background())
 		if err != nil {
 			panic(err)
 		}
 
 		fmt.Println("all orders cancelled")
-	}()
+	}(client)
 
-	//targetTime := time.Unix(TargetTimestamp, 0)
-	//
-	//if targetTime.Before(now) {
-	//	fmt.Println("target timestamp has already been passed")
-	//	return
-	//}
-	//
-	//delta := targetTime.Sub(now)
-	//fmt.Println("sleeping:", delta)
-	//time.Sleep(delta)
-
-	// place sell order
-	// print time delta result
-	//orderUpdateTime := time.Unix(order.UpdateTime, 0)
-	//delta := orderUpdateTime.Sub(now) // targetTime
-	//fmt.Println("time difference in ns:", order.UpdateTime-nowTs, order.UpdateTime, nowTs)
 	var sum int64
 	results := make([]int64, 0, 20)
 
 	for i := 0; i < 20; i++ {
-		res := sendOrder()
+		res := sendOrder(client)
 		sum += res
 		results = append(results, res)
 	}
 
-	fmt.Printf("average time difference is (ms): %f\n", float64(float64(sum)/20.0))
+	fmt.Printf("average time difference is (ms): %f\n", float64(sum)/20.0)
 	fmt.Println(results)
 }
 
-func sendOrder() int64 {
+func sendOrder(client *futures.Client) int64 {
 	now := time.Now()
 	nowTs := now.UnixMilli()
-	client := binance.NewFuturesClient(ApiKey, ApiSecret)
 	quantity := fmt.Sprintf("%d", OrderSize)
 	price := fmt.Sprintf("%d", OrderPrice)
 	order, err := client.NewCreateOrderService().
@@ -80,5 +60,5 @@ func sendOrder() int64 {
 		panic(err)
 	}
 
-	return order.UpdateTime-nowTs
+	return order.UpdateTime - nowTs
 }
